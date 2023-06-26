26483a70727f6527379d0ed0104949ca7be7e093
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-26 14:11:34 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,8 @@
-hdgbjhnlfhbjkvnl
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+}
