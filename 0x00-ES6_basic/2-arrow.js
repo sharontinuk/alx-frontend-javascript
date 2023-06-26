26ce1e8c1bd2f06e9b2b0ce303b6b358f94e0fa8
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-26 13:30:07 +0100

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,1 +1,9 @@
-gbhn,jcbhnmv,.m,
+export default class getNeighborhoodsList {
+  constructor() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    this.addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
+}
