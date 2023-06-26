5d04c0918a27dd78f267e191eeb8eaf85c070154
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-26 14:07:16 +0100

    Update 9-main.js

--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -1,1 +1,6 @@
-vhfgndmdgfgh,b,jfdgvhgsdvgsdh
+import getFullBudgetObject from './9-getFullBudget.js';
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
