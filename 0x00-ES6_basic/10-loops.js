49ecb64176cc71fdc59fb4b8666521645d3eed75
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-26 14:07:58 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-jdhjnll;f;mkjhkf
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+  }
+
+  return newArray;
+}
