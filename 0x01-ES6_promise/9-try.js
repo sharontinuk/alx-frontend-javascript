370f29aa1178b9034ee9b4555310450eb44b5354
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-28 22:15:40 +0100

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-qwerty
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let mathFunc;
+
+  try {
+    mathFunc = mathFunction();
+  } catch (error) {
+    mathFunc = error.toString();
+  }
+  queue.push(mathFunc);
+  queue.push('Guardrail was processed');
+  return queue;
+}
