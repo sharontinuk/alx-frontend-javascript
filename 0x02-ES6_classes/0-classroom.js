282fe2b122ee115cc078ed542d000d35d8289685
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-29 22:24:45 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,1 +1,17 @@
-hgvdfg h,,jknbjhv jn,kjmjnbhchbjnkc
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-underscore-dangle */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  set maxStudentsSize(newMaxStudentsSize) {
+    if (typeof newMaxStudentsSize === 'number') {
+      this._maxStudentsSize = newMaxStudentsSize;
+    }
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+}
