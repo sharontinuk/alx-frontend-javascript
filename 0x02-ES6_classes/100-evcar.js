aab6cd4ea7db997eb3cf65aa0ea53b1c5a649c6a
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-06-29 22:36:10 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,14 @@
-gvbjhkjnlmjndhb
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
