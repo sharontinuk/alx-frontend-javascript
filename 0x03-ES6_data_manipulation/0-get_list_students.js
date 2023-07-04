fee13b90811eabdc4e2123490615539ae3a437e9
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-07-04 13:38:30 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,7 @@
-fn,mgjdfgnk .mks/df.dkmsnfk
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
